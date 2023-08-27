8d389b0c9c49c241d7bcc2b506c4df1d3699de86
ofek ashkenazi <ofeka18@gmail.com>
2023-08-27 15:43:41 +0300

    implant the background gradient and fix the z-index so he can be behind the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Board from "@/components/board";
 import Header from "@/components/header";
 
 export default function Home() {
@@ -5,8 +6,9 @@ export default function Home() {
 
     <main>
       <Header/>
-      <h1>Trello 2.0 AI clone</h1>
+
+      <Board />
     </main>
-    
+
   )
 }
