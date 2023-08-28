6ad9f08b6894dc68a88f941c5a3ec9ed6f524ebe
ofek ashkenazi <ofeka18@gmail.com>
2023-08-28 18:55:29 +0300

    work on add a msg from chatgpt and make function for flat the object map of the board and connact it to header cmp, but the open ai config doesnt work need to fix this.

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,35 @@
 'use client'
-{/* im using client component for react avatar */ }
 
 import Image from "next/image";
 import TrelloLogo from "@/images/Trello_logo.png"
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import { useEffect, useState } from "react";
+import fetchSuggestion from "@/lib/fetchSuggestion";
 
 export default function Header() {
-    const [searchString, setSearchString] = useBoardStore((state) => [
+
+    const [board, searchString, setSearchString] = useBoardStore((state) => [
+        state.board,
         state.searchString,
-        state.setSearchString
+        state.setSearchString,
     ])
+
+    const [loading, setLoading] = useState<boolean>(false)
+    const [suggestion,setSuggestion] = useState<string>("")
+
+    useEffect(() => {
+        if (board.columns.size === 0) return
+        setLoading(true)
+
+        const fetchSuggestionFunc = async () => {
+            const suggestion = await fetchSuggestion(board)
+            setSuggestion(suggestion)
+            setLoading(false)
+
+        }
+    }, [board])
 
 
 
@@ -58,9 +76,8 @@ export default function Header() {
             <div className="flex items-center justify-center px-5 py-2 md:py-5 ">
 
                 <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] ">
-                    <UserCircleIcon className="inline-block h-10 w-10 mr-1 text-[#0055D1]" />
-                    GPT is summarising your tasks for the day
-
+                    <UserCircleIcon className={`inline-block h-10 w-10 mr-1 text-[#0055D1] ${loading && "animate-pulse"} `} />
+                    {suggestion && !loading ? suggestion : "GPT is summarising your tasks for the day"}
                 </p>
 
             </div>
---
+++ lib/fetchSuggestion.ts
@@ -0,0 +1,17 @@
+import formatTodoForAi from "./formatTodoForAi"
+
+
+export default async function fetchSuggestion(board: Board) {
+    const todos = formatTodoForAi(board)
+    const res = await fetch("/api/generateSummary", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({ todos }),
+    })
+
+    const GPTdata = await res.json()
+    const { content } = GPTdata
+    return content
+} 
