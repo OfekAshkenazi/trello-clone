e3aff9cba1ae594dbfcc6eed869f415cbea44656
ofek ashkenazi <ofeka18@gmail.com>
2023-08-28 19:28:11 +0300

    implant delete function in the optimistic way so that first the user will see the change right away and then it will sent a request to the database for delete the document.

--- components/todoCard.tsx
+++ components/todoCard.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useBoardStore } from "@/store/BoardStore";
 import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
     DraggableProvidedDragHandleProps,
@@ -8,7 +9,7 @@ import {
 
 type Props = {
     todo: Todo,
-    index: Number,
+    index: number,
     id: TypedColumn,
     innerRef: (element: HTMLElement | null) => void,
     draggableProps: DraggableProvidedDraggableProps,
@@ -16,6 +17,9 @@ type Props = {
 }
 
 export default function TodoCard({ todo, index, id, innerRef, draggableProps, dragHandleProps }: Props) {
+
+    const deleteTask = useBoardStore((state) => state.deleteTask)
+    
     return (
         <div
             {...draggableProps} {...dragHandleProps} ref={innerRef}
@@ -24,12 +28,12 @@ export default function TodoCard({ todo, index, id, innerRef, draggableProps, dr
 
             <div className="flex justify-between items-center p-5">
                 <p>{todo.title}</p>
-                <button className="text-red-500 hover:text-red-600">
+                <button onClick={() => deleteTask(index, todo, id)}
+                 className="text-red-500 hover:text-red-600">
                     <XCircleIcon
                         className="ml-5 h-8 w-8"
                     />
                 </button>
-
 
             </div>
 
